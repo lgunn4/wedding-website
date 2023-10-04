6ae2ffecd6072207b6d0252d98972ed023b84a91
Logan Gunn <44077842+lgunn4@users.noreply.github.com>
2023-10-04 14:47:45 -0400

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module WeddingWebsite
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.hosts << "loganandbri.love"
   end
 end
