80a66fe5dae512da528a4c5dd3ec8426cd65feae
Logan Gunn <lgunn4@uwo.ca>
2023-10-05 21:01:07 -0400

    still trying to get form working

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,30 +1,18 @@
 # app/controllers/guests_controller.rb
 class GuestsController < ApplicationController
-  before_action :set_guest, only: %i[show edit update destroy]
-
   def create
     @guest = Guest.new(guest_params)
 
     if @guest.save
-      render turbo_stream: turbo_stream.replace(
-        'body', partial: 'shared/thank_you_modal'
-      )
+      redirect_to root_path, notice: 'Thank you for your RSVP!'
     else
-      render json: { success: false, errors: @guest.errors.full_messages }
+      render :home
     end
   end
 
   private
 
-  def set_guest
-    @guest = Guest.find(params[:id])
-  end
-
   def guest_params
-    params.require(:guest).permit(
-      :name, :email, :number_of_guests,
-      address_attributes: %i[street line_2 city province postal_code country],
-      song_requests_attributes: %i[id title artist _destroy]
-    )
+    params.require(:guest).permit(:name, :email, :number_of_guests, address_attributes: [:street, :line_2, :city, :province, :postal_code, :country], song_requests_attributes: [:title, :artist])
   end
 end
