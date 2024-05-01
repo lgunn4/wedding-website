8e08bb72e7721e03508ef7aec170db95a4406039
Logan Gunn <lgunn4@uwo.ca>
2024-05-01 10:33:02 -0400

    Removed guest on home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @guest = Guest.new
-    @guest.build_address
-    @guest.song_requests.build
+    # Uncomment when we add the RSVP form back
+    # @guest = Guest.new
+    # @guest.build_address
+    # @guest.song_requests.build
   end
 end
