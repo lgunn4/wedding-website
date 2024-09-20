36bc46285fdac3bcd2a5a5782119f2066f8195c2
Logan Gunn <44077842+lgunn4@users.noreply.github.com>
2024-09-20 09:15:27 -0700

    🐞 Debug mode

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
   def index
-    # Uncomment when we add the RSVP form back
-    # @guest = Guest.new
-    # @guest.build_address
-    # @guest.song_requests.build
+    @debug_rsvp = params[:debug] == 'rsvp'
+
+    if @debug_rsvp
+      @guest = Guest.new
+      @guest.build_address
+      @guest.song_requests.build
+    end
   end
 end
