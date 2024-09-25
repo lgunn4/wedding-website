1c97283e81e5642759a92d2ce8c43173f4013cd1
Logan Gunn <lgunn4@uwo.ca>
2024-09-25 15:40:32 +0000

    👨🏻‍💻 Made development better

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,12 +77,13 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: Rails.application.credentials.mailer.email,
-    password: Rails.application.credentials.mailer.password,
+    user_name: "email@email.com",
+    password: "password",
     authentication: 'plain',
     enable_starttls_auto: true
   }
 
   config.action_mailer.logger = ActiveSupport::Logger.new(STDOUT)
   config.action_mailer.logger.level = Logger::DEBUG
+  config.hosts << "code-rails.loft-cloud.com"
 end
