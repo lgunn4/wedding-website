1875730b1dfb62e0563eb08807f98c26ffb96ff7
Logan Gunn <lgunn4@uwo.ca>
2024-09-27 16:07:36 +0000

    nevermind no point

--- db/migrate/20240923033819_add_rsvp_ref_to_guests.rb
+++ db/migrate/20240923033819_add_rsvp_ref_to_guests.rb
@@ -1,5 +1,5 @@
 class AddRsvpRefToGuests < ActiveRecord::Migration[7.0]
   def change
-    add_reference :guests, :rsvp, null: true, foreign_key: true
+    add_reference :guests, :rsvp, null: false, foreign_key: true
   end
 end
--- db/migrate/20240923033823_add_rsvp_ref_to_addresses.rb
+++ db/migrate/20240923033823_add_rsvp_ref_to_addresses.rb
@@ -1,5 +1,5 @@
 class AddRsvpRefToAddresses < ActiveRecord::Migration[7.0]
   def change
-    add_reference :addresses, :rsvp, null: true, foreign_key: true
+    add_reference :addresses, :rsvp, null: false, foreign_key: true
   end
 end
