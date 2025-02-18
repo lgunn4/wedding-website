ef35dafa57ae47615218c357ad8f132a5622f9d6
Logan Gunn <lgunn4@uwo.ca>
2025-02-18 16:05:22 +0000

    💌 RSVP content update

--- app/mailers/rsvp_mailer.rb
+++ app/mailers/rsvp_mailer.rb
@@ -3,12 +3,12 @@
 class RsvpMailer < ApplicationMailer
   def confirmation_email(rsvp)
     @rsvp = rsvp
-    mail(to: @rsvp.email, subject: '💌 Thanks! Your RSVP is confirmed')
+    mail(to: @rsvp.email, subject: '💌 Thank you! We have received your RSVP')
   end
 
   def alert_email(rsvp)
     @rsvp = rsvp
     @number_of_guests = Guest.all.count
-    mail(to: User.all.map(&:email), subject: 'A new RSVP has been submitted!')
+    mail(to: User.all.map(&:email), subject: 'A new RSVP has been submitted')
   end
 end
