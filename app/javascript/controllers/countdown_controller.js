fdcdd3b1443e9b950f8c1afb6a5f45833213c157
Logan Gunn <lgunn4@uwo.ca>
2025-02-19 02:47:11 +0000

    ⏰ Updated countdown timer

--- app/javascript/controllers/countdown_controller.js
+++ app/javascript/controllers/countdown_controller.js
@@ -1,25 +1,23 @@
 import { Controller } from "@hotwired/stimulus";
 
-var countDownDate = new Date("Jun 14, 2025 15:00:00").getTime();
+// Specify the countdown target date and time in UTC
+var countDownDate = Date.UTC(2025, 5, 14, 20, 0, 0); // Months are zero-based: 5 for June; 20 UTC equates to 4pm EST in June
 
 // Connects to data-controller="countdown"
 export default class extends Controller {
   static targets = ["countdown"];
 
   connect() {
-    const now = new Date().getTime();
-    this.endTime = countDownDate
-
-    this.countdown = setInterval(this.countdown.bind(this), 250);
+    this.countdown = setInterval(this.updateCountdown.bind(this), 250);
   }
 
-  countdown() {
-    const now = new Date();
-    const secondsRemaining = (this.endTime - now) / 1000;
+  updateCountdown() {
+    const now = Date.now(); // Current time in UTC
+    const secondsRemaining = (countDownDate - now) / 1000;
 
     if (secondsRemaining <= 0) {
       clearInterval(this.countdown);
-      this.countdownTarget.innerHTML = ''
+      this.countdownTarget.innerHTML = '';
       return;
     }
 
@@ -28,12 +26,8 @@ export default class extends Controller {
     const secondsPerMinute = 60;
 
     const days = Math.floor(secondsRemaining / secondsPerDay);
-    const hours = Math.floor(
-      (secondsRemaining % secondsPerDay) / secondsPerHour
-    );
-    const minutes = Math.floor(
-      (secondsRemaining % secondsPerHour) / secondsPerMinute
-    );
+    const hours = Math.floor((secondsRemaining % secondsPerDay) / secondsPerHour);
+    const minutes = Math.floor((secondsRemaining % secondsPerHour) / secondsPerMinute);
     const seconds = Math.floor(secondsRemaining % secondsPerMinute);
 
     this.countdownTarget.innerHTML = `<strong>${days}</strong> days, <strong>${hours}</strong> hours, <strong>${minutes}</strong> minutes, <strong>${seconds}</strong> seconds`;
