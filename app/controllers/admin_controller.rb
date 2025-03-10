94e213c9de79a312759fb871c3ca7e9978b151f6
Logan Gunn <lgunn4@uwo.ca>
2025-03-10 14:50:14 +0000

    👨🏻‍🏫 Better Logic

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,9 @@ class AdminController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @guests = Rsvp.attending_guests
+    @guests = Rsvp.where(complete: true).includes(:guests).flat_map(&:guests)
+    
+    @attending_guests = Rsvp.attending_guests.count
     @song_requests = Rsvp.attending_song_requests
     @bus_guests = Rsvp.attending_bus_guests
   end
