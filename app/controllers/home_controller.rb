8216c4e3eebb3b731ec6ca6010eefebd50b9cfed
Logan Gunn <lgunn4@uwo.ca>
2025-03-11 15:16:54 +0000

    🚀 Shipping RSVP form!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   layout 'styled'
   def index
-    @debug_rsvp = params[:debug] == 'rsvp'
     @show_modal = session.delete(:show_modal) 
     @attending = session.delete(:attending) 
   end
